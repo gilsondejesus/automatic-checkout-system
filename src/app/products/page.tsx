c6c4937148b0312cd19db0c6c28ca355fce910ea
Gilson de Jesus <gilsonmdj@gmail.com>
2025-02-19 12:53:07 -0300

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,7 +3,7 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return ( 
-    <div className="bg-gray-300 border-red-500 border-2 p-5 rounded-xl border-red-500 flex flex-col items-center justify-center mx-auto">
+    <div className="bg-gray-300 border-red-500 border-2 p-5 rounded-xl flex flex-col items-center justify-center mx-auto">
       <h1 className=" text-red-600 font-bold">Product Page</h1>
       <Button>Comprar</Button>
       <Input placeholder="Buscar productos" />
